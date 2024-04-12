7ae56c72bcf09f4abae9eeb0aedf42ab3dfac24f
Davide Lodde <johndoe@example.com>
2024-04-12 18:56:20 +0200

    modifiche stile

--- LiveProject/src/component/Home.jsx
+++ LiveProject/src/component/Home.jsx
@@ -1,48 +1,50 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 export function Home() {
-    const [result, setResult] = useState(null)
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-            .then(res => res.json())
-            .then(json => {
-                setResult(json)
-            });
+  const [result, setResult] = useState(null);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        setResult(json);
+      });
+  }, []);
+  console.log(result);
 
-    }, [])
-    console.log(result);
+  const styleDiv = {
+    width: "100vw",
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '2vw'
     
-    const styleDiv = {
-        width: '100vw',
-        
+  };
 
-    }
+  const styleCard = {
+    width: '23vw',
+    display: 'flex',
+    alignItems: 'center'
+  };
 
-    const styleCard = {
-        width: '25%',
-    }
-
-
-    return (
-        
+  return (
     <div style={styleDiv}>
-    {result && result.map((prodotto) => 
-        <Card style={styleCard} key={prodotto.id}>
-        <Card.Img variant="top" src={prodotto.image} />
-        <Card.Body>
-          <Card.Title>
-            {prodotto.title}<br/>
-            {prodotto.price}€
-            </Card.Title>
-          <Card.Text>{prodotto.description}</Card.Text>
-          <Button variant="primary">Acquista</Button>
-        </Card.Body>
-      </Card>
-    )}
-      
+      {result &&
+        result.map((prodotto) => (
+          <Card style={styleCard} key={prodotto.id}>
+            <Card.Img style={{height: '10vh', width: '10vh'}} variant="top" src={prodotto.image} />
+            <Card.Body>
+              <Card.Title>
+                {prodotto.title}
+                <br />
+                {prodotto.price}€
+              </Card.Title>
+              <Card.Text>{prodotto.description}</Card.Text>
+              <Button variant="primary">Acquista</Button>
+            </Card.Body>
+          </Card>
+        ))}
     </div>
-    )
+  );
 }
